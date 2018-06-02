de006006ffd699f86336967e9011bd50754f6aaf
Josh Abraham <sinisterpatrician@gmail.com>
2018-06-02 11:51:27 -1000

    Initial commit for hw3

---
+++ hw3/hw3.cpp
@@ -0,0 +1,20 @@
+#include <set>
+
+using namespace std;
+
+class MinSpanningTree {
+
+    private:
+        set<int> tree;
+
+    public:
+        MinSpanningTree()
+        {
+        }
+};
+
+int main(int argc, char **argv)
+{
+    MinSpanningTree m;
+    return 0;
+}
